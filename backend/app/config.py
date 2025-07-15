bb3d85271c58c6fb049bb8391e51801300e238a2
caitouyangge <78825426@qq.com>
2025-07-15 18:28:55 +0800

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -21,10 +21,6 @@ class Config:
     MYSQL_PORT = 3306
     MYSQL_USER = 'root'
     MYSQL_PASSWORD = 'ok'
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
     MYSQL_DB = 'realtime_monitoring'
     MYSQL_CHARSET = 'utf8mb4'
 
